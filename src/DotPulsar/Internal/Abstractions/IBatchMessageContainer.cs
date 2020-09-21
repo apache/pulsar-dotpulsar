cc7f8932fc55c4f2b9e46d3a7c7c88475c954b72
AaronRobert <Robert.Aaron2017@outlook.com>
2020-09-21 16:39:55 +0800

    Add BatchMessageContainer.

---
+++ src/DotPulsar/Internal/Abstractions/IBatchMessageContainer.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace DotPulsar.Internal.Abstractions
+{
+    using System.Collections.Generic;
+
+    interface IBatchMessageContainer
+    {
+        MessageMetadata MessageMetadata { get;}
+        Queue<Message> Messages { get; }
+        /// <summary>
+        /// Add message to the batch message container.
+        /// </summary>
+        /// <param name="message">The message will be added to the batch message container</param>
+        /// <returns>True if the batch is full, otherwise false</returns>
+        bool Add(Message message);
+
+        /// <summary>
+        /// Check the batch message container have enough space for the message want to add.
+        /// </summary>
+        /// <param name="message">the message want to add</param>
+        /// <returns>return true if the container have enough space for the specific message, otherwise return false.</returns>
+        bool HaveEnoughSpace(Message message);
+
+        /// <summary>
+        /// Get count of messages in the message batch container.
+        /// </summary>
+        /// <returns>message count</returns>
+        //int GetNumMessagesInBatch();
+
+        /// <summary>
+        /// Check the message batch container is empty.
+        /// </summary>
+        /// <returns>return true if empty, otherwise return false.</returns>
+        bool IsEmpty();
+
+        /// <summary>
+        /// Clear the message batch container.
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ src/DotPulsar/Internal/BatchMessageContainer.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace DotPulsar.Internal
+{
+    using DotPulsar.Internal.Abstractions;
+    using System.Buffers;
+    using System.Collections.Generic;
+
+    public sealed class BatchMessageContainer : IBatchMessageContainer
+    {
+        public MessageMetadata MessageMetadata {  get; private set; } = new MessageMetadata();
+        public Queue<Message> Messages { get; private set; } = new Queue<Message>();
+        private SequenceBuilder<byte> payload = new SequenceBuilder<byte>();
+        private int maxMessagesInBatch = 0;
+        private int numMessagesInBatch = 0;
+        private long maxBatchBytesSize = 0;
+        private long currentBatchSize = 0;
+        private ulong highestSequenceId = 0;
+        
+        public bool Add(Message message)
+        {
+            if (++numMessagesInBatch == 1)
+            {
+                MessageMetadata.SequenceId = message.SequenceId;
+            }
+
+            currentBatchSize += message.Data.Length;
+            Messages.Enqueue(message);
+
+            highestSequenceId = message.SequenceId;
+
+            return IsBatchFull();
+        }
+
+        private bool IsBatchFull()
+        {
+            return (maxBatchBytesSize > 0 && currentBatchSize >= maxBatchBytesSize)
+                || (maxMessagesInBatch > 0 && numMessagesInBatch >= maxMessagesInBatch);
+        }
+
+        public void Clear()
+        {
+            MessageMetadata = new MessageMetadata();
+            Messages.Clear();
+            payload = new SequenceBuilder<byte>();
+            numMessagesInBatch = 0;
+            currentBatchSize = 0;
+        }
+
+        public int GetNumMessagesInBatch()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HaveEnoughSpace(Message message)
+        {
+            var messageSize = message.Data.Length;
+            return (maxBatchBytesSize > 0 && (currentBatchSize + messageSize) >= maxBatchBytesSize)
+                || (maxMessagesInBatch > 0 && (numMessagesInBatch + 1) >= maxMessagesInBatch);
+        }
+
+        public bool IsEmpty()
+        {
+            return currentBatchSize == 0;
+        }
+    }
+}
