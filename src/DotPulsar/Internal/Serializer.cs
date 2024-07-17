6448b847099001af2af749cf7cf047cc781cb4a6
Daniel Blankensteiner <db@vmail.dk>
2024-07-17 09:02:41 +0200

    Let's see if this fixes the bug in protobuf-net

--- src/DotPulsar/Internal/Serializer.cs
+++ src/DotPulsar/Internal/Serializer.cs
@@ -19,6 +19,8 @@ using System.Buffers;
 
 public static class Serializer
 {
+    static Serializer() => Serialize(new BaseCommand()); //Let's see if this fixes the bug in protobuf-net
+
     public static T Deserialize<T>(ReadOnlySequence<byte> sequence) => ProtoBuf.Serializer.Deserialize<T>(sequence);
 
     public static ReadOnlySequence<byte> Serialize(BaseCommand command)
